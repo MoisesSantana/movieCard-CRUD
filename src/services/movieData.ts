8242fa6cfe9d868ffbbb7548daf8170ca8e9746f
Moisés Santana <moisaant@gmail.com>
2022-07-04 02:27:26 -0300

    fix style

--- src/services/movieData.ts
+++ src/services/movieData.ts
@@ -6,8 +6,7 @@ const movies = [
     storyline:
       "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
     rating: 4.5,
-    imagePath:
-      "https://www.portallos.com.br/wp-content/uploads/2016/12/Kingsglaive-FFXV-009.jpg",
+    imagePath: "images/Kingsglaive_Final_Fantasy_XV.jpg",
     bookmarked: true,
     genre: "action",
   },
@@ -18,7 +17,7 @@ const movies = [
     storyline:
       "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
     rating: 4.5,
-    imagePath: "https://miro.medium.com/max/1400/0*p1EOnpl55C7s4Ovm.jpg",
+    imagePath: "images/Final_Fantasy_Spirits_Within.jpg",
     bookmarked: false,
     genre: "fantasy",
   },
@@ -29,8 +28,7 @@ const movies = [
     storyline:
       "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
     rating: 5,
-    imagePath:
-      "https://aventurasnahistoria.uol.com.br/media/_versions/legacy/2017/03/30/ghost-in-the-shell_widexl.jpg",
+    imagePath: "images/Ghost_In_The_Shell_2_0.jpg",
     bookmarked: false,
     genre: "comedy",
   },
@@ -41,8 +39,7 @@ const movies = [
     storyline:
       "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope, the legendary city of Olympus.",
     rating: 3.8,
-    imagePath:
-      "https://i1.wp.com/www.jbox.com.br/wp/wp-content/uploads/2016/03/appeseedalpha.jpg?fit=680%2C430&quality=99&strip=all&ssl=1",
+    imagePath: "images/Appleseed_Alpha.jpg",
     bookmarked: true,
     genre: "action",
   },
@@ -53,7 +50,7 @@ const movies = [
     storyline:
       "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
     rating: 4.2,
-    imagePath: "https://i.playground.ru/p/wFAzIr-ykyjva1XUkqA17w.jpeg",
+    imagePath: "images/Resident_Evil_Vendetta.jpg",
     bookmarked: true,
     genre: "fantasy",
   },
